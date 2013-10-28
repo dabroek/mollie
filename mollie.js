dabroek/mollie

commit 43aa0e5f1cd889a00a1d229129b9a641da4436bd

Added request failed error

mollie.js
@@ -319,6 +319,12 @@ mollie.talk = function( path, fields, callback ) {
 		})
 	})
 	
+	response.on( 'error', function( error ) {
+		var err = new Error('request failed')
+		err.error = error
+		doCallback( err )
+	})
+	
 	// post and close
 	request.end( query )
 	

commit 63cc99151ef098ffb4524585c3bc61998ac249dc

Fixed request failed error
introduced in 43aa0e5f1cd889a00a1d229129b9a641da4436bd

mollie.js
@@ -319,7 +319,7 @@ mollie.talk = function( path, fields, callback ) {
 		})
 	})
 	
-	response.on( 'error', function( error ) {
+	request.on( 'error', function( error ) {
 		var err = new Error('request failed')
 		err.error = error
 		doCallback( err )
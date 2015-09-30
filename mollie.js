ca5272dfe9e6c6b2fae183bbdf4db4d80a62d6e0
Franklin van de Meent <fr@nkl.in>
2015-09-30 05:46:16 +0200

    Rewrite code and methods
    Interface changed:
    
    * All methods removed
    
    * New Mollie payment API methods added

--- mollie.js
+++ mollie.js
@@ -6,340 +6,115 @@ Feedback:      https://github.com/fvdm/nodejs-mollie/issues
 License:       Public Domain / Unlicense (see UNLICENSE file)
 */
 
-// MODULE
+var httpreq = require ('httpreq');
 
-var mollie = {}
-var https = require('https')
-var querystring = require('querystring')
-var xml2json = require('node-xml2json')
+function talk (method, path, params, callback) {
+  var options = {
+    url: 'https://api.mollie.nl/v1' + path,
+    method: method,
+    parameters: params || null,
+    timeout: parseInt (this.config.timeout, 10) || 5000,
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ' + this.config.apikey,
+      'User-Agent': 'mollie.js (https://www.npmjs.com/package/mollie)'
+    }
+  };
 
-mollie.api = {
-	partnerid:	0,
-	username:	'',
-	password: 	''
+  httpreq.doRequest (options, function (err, res) {
+    var data = res && res.body || null;
+    var error = null;
+
+    if (err) {
+      error = new Error ('request failed');
+      error.error = err;
+    } else {
+      try {
+        data = JSON.parse (data);
+
+        if (data.error) {
+          error = new Error ('API error');
+          error.error = data.error;
+          data = null;
+        }
+      } catch (e) {
+        error = new Error ('invalid response');
+        error.error = e;
+      }
+    }
+
+    if (method === 'DELETE' && res && res.statusCode === 204) {
+      error = null;
+      data = true;
+    }
+
+    if (error) {
+      error.statusCode = res && res.statusCode || null;
+      error.headers = res && res.headers || {};
+    }
+
+    callback (error, data);
+  });
 }
 
-// Padding for the four digit bank_ids 
-String.prototype.lpad = function( chr, count ) {
-	var str = this
-	while( str.length < count ) {
-		str = chr + str
-	}
-	return str
-}
-
-// Account credits
-mollie.credits = function( callback ) {
-	mollie.talk(
-		'credits',
-		{
-			username:	mollie.api.username,
-			password:	mollie.api.password
-		},
-		callback
-	)
-}
-
-
-// HLR-lookup (Network Query)
-// https://www.mollie.nl/beheer/sms-diensten/documentatie/hlr/
-// Codes: http://en.wikipedia.org/wiki/Mobile_Network_Code
-mollie.hlr = function( vars, callback ) {
-	vars.username = mollie.api.username
-	vars.password = mollie.api.password
-	mollie.talk( 'hlr', vars, callback )
-}
-
-
-// SMS
-// Normal: https://www.mollie.nl/beheer/sms-diensten/documentatie/sms/http/
-// Premium: https://www.mollie.nl/beheer/sms-diensten/documentatie/sms/http/?s=premium
-mollie.sms = function( vars, callback ) {
-	vars.username = mollie.api.username
-	vars.password = mollie.api.password
-	mollie.talk( 'sms', vars, callback )
-}
-
-
-// Phone / IVR / 090x
-// https://www.mollie.nl/beheer/betaaldiensten/documentatie/ivr/
-mollie.ivr = {
-	
-	// payment
-	payment: function( vars, callback ) {
-		
-		vars.a = 'fetch'
-		vars.partnerid = mollie.api.partnerid
-		mollie.talk( 'micropayment', vars, callback )
-		
-	},
-	
-	// check
-	check: function( vars, callback ) {
-		
-		vars.a = 'check'
-		mollie.talk( 'micropayment', vars, callback )
-		
-	}
-	
+function fixcbparams () {
+  if (typeof params === 'function') {
+    callback = params;
+    params = {};
+  }
 }
 
 
+var Mollie = function (config) {
+  this.config = config || {};
+  this.talk = talk;
 
-// paysafecard
-// https://www.mollie.nl/beheer/betaaldiensten/documentatie/paysafecard/
-mollie.paysafecard = {
-	
-	// prepare
-	prepare: function( vars, callback ) {
-		
-		vars.partnerid = mollie.api.partnerid
-		
-		// fix centen
-		if( typeof vars.amount === 'string' && vars.amount.indexOf('.') ) {
-			var asplit = vars.amount.split('.')
-			if( asplit[1] !== undefined ) {
-				vars.amount = vars.amount * 100
-			}
-		}
-		
-		mollie.talk( 'paysafecard/prepare', vars, callback )
-		
-	},
-	
-	
-	// status
-	status: function( vars, callback ) {
-		vars.partnerid = mollie.api.partnerid
-		mollie.talk( 'paysafecard/check-status', vars, callback )
-	}
-	
-}
+  this.payments = {
+    get: function (id, callback) {
+      this.talk ('GET', '/payments/' + id, {}, callback);
+    },
 
+    list: function (params, callback) {
+      fixcbparams ();
+      this.talk ('GET', '/payments', params, callback);
+    },
 
-// iDEAL
-// https://www.mollie.nl/beheer/betaaldiensten/documentatie/ideal/
-mollie.ideal = {
-	
-	// link
-	paymentLink: function( vars, callback ) {
-		
-		vars = typeof vars !== 'object' ? {} : vars
-		vars.a = 'create-link'
-		vars.partnerid = mollie.api.partnerid
-		
-		// fix centen
-		if( typeof vars.amount == 'string' && vars.amount.indexOf('.') ) {
-			var asplit = vars.amount.split('.')
-			if( asplit[1] !== undefined ) {
-				vars.amount = vars.amount * 100
-			}
-		}
-		
-		// post
-		mollie.talk( 'ideal', vars, callback )
-		
-	},
-	
-	
-	// payment
-	payment: function( vars, callback ) {
-		
-		vars = typeof vars !== 'object' ? {} : vars
-		vars.a = 'fetch'
-		vars.partnerid = mollie.api.partnerid
-		
-		// fix centen
-		if( typeof vars.amount == 'string' && vars.amount.indexOf('.') ) {
-			var asplit = vars.amount.split('.')
-			if( asplit[1] !== undefined ) {
-				vars.amount = vars.amount * 100
-			}
-		}
-		
-		// post
-		mollie.talk( 'ideal', vars, callback )
-		
-	},
-	
-	
-	// check
-	check: function( vars, callback ) {
-		
-		vars = typeof vars !== 'object' ? {} : vars
-		vars.a = 'check'
-		vars.partnerid = mollie.api.partnerid
-		
-		// fix testmode
-		if( vars.testmode !== undefined ) {
-			vars.testmode = vars.testmode === true || vars.testmode === 'true' ? 'true' : 'false'
-		}
-		
-		// request
-		mollie.talk( 'ideal', vars, function( err, res ) {
-			if( err ) {
-				callback( err )
-			} else {
-				res = res.order
-				res.payed = res.payed === 'true' ? true : false
-				if( res.consumer && res.consumer.consumercity === 'NOT PROVIDED' ) {
-					res.consumer.consumercity = null
-				}
-				callback( null, res )
-			}
-		})
-		
-	},
-	
-	// banklist
-	banklist: function( testmode, callback ) {
-		
-		// fix callback
-		if( callback === undefined ) {
-			var callback = testmode
-			testmode = -1
-		}
-		
-		// vars
-		var vars = {
-			a: 'banklist'
-		}
-		
-		if( testmode !== -1 ) {
-			vars.testmode = testmode ? 'true' : 'false'
-		}
-		
-		// request
-		mollie.talk( 'ideal', vars, function( err, res ) {
-			if( err ) {
-				callback( err )
-			} else {
-				var banks = {}
-				if( res && res.bank ) {
-					if( res.bank.bank_id === undefined ) {
-						for( var b in res.bank ) {
-							bank = res.bank[b]
-							bank.bank_id = bank.bank_id.toString()
-							if( bank.bank_id.length < 4 ) {
-								bank.bank_id = bank.bank_id.lpad( "0", 4 )
-							}
-							banks[ bank.bank_id ] = bank
-						}
-					} else {
-						res.bank.bank_id = res.bank.bank_id.toString()
-						if( res.bank.bank_id.length < 4 ) {
-							res.bank.bank_id = res.bank.bank_id.lpad( "0", 4 )
-						}
-						banks[ res.bank.bank_id ] = res.bank
-					}
-				}
-				callback( null, banks )
-			}
-		})
-	}
-}
+    create: function (params, callback) {
+      this.talk ('POST', '/payments', params, callback);
+    }
+  };
 
+  this.refunds = {
+    list: function (paymentId, params, callback) {
+      fixcbparams ();
+      this.talk ('GET', '/payments/' + paymentId + '/refunds', {}, callback);
+    },
 
-// talk
-mollie.talk = function( path, fields, callback ) {
-	
-	// fix input
-	if( typeof fields === 'function' ) {
-		var callback = fields
-		var fields = {}
-	}
-	
-	// prevent multiple callbacks
-	var complete = false
-	function doCallback( err, res ) {
-		if( ! complete ) {
-			complete = true
-			callback( err, res )
-		}
-	}
-	
-	// query string
-	var query = typeof fields === 'object' ? querystring.stringify( fields ) : ''
-	
-	// build request
-	var options = {
-		host: 'www.mollie.nl',
-		port: 443,
-		path: '/xml/'+ path,
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'Content-Length': query.length
-		}
-	}
-	
-	var request = https.request( options )
-	
-	// response
-	request.on( 'response', function( response ) {
-		var data = []
-		var size = 0
-		
-		response.on( 'data', function( chunk ) {
-			data.push(chunk)
-			size += chunk.length
-		})
-		
-		response.on( 'close', function() {
-			doCallback( new Error('disconnected') )
-		})
-		
-		response.on( 'end', function() {
-			var error = null
-			
-			// combine chunks
-			var buf = new Buffer(size)
-			var pos = 0
-			for( var d in data ) {
-				data[d].copy( buf, pos )
-				pos += data[d].length
-			}
-			
-			data = data.toString('utf8').trim()
-			
-			// process response
-			if( data === '' ) {
-				error = new Error('no response data')
-			} else {
-				// Remove leading zero's in bank_id's so xml2json won't parse
-				// then as octal numbers:
-				data = data.replace( />0+(\d+)</g, ">$1<" )
-				
-				data = xml2json.parser( data.trim() )
-				data = data.response
-				
-				if( data.item !== undefined ) {
-					data = data.item
-				}
-				
-				// catch API errors
-				if( data.resultcode > 10 || data.type === 'error' ) {
-					error = new Error('API error')
-					error.code = data.resultcode || data.errorcode
-					error.error = data.resultmessage || data.message
-				}
-			}
-			
-			// return result
-			doCallback( error, data )
-		})
-	})
-	
-	request.on( 'error', function( error ) {
-		var err = new Error('request failed')
-		err.error = error
-		doCallback( err )
-	})
-	
-	// post and close
-	request.end( query )
-	
-}
+    create: function (paymentId, params, callback) {
+      fixcbparams ();
+      this.talk ('POST', '/payments/' + paymentId + '/refunds', params, callback);
+    },
 
-// ready
-module.exports = mollie
+    delete: function (paymentId, refundId, callback) {
+      this.talk ('DELETE', '/payments/' + paymentId + '/refunds/' + refundId, {}, callback);
+    }
+  };
+
+  this.issuers = {
+    get: function (issuerId, callback) {
+      this.talk ('GET', '/issuers/' + issuerId, {}, callback);
+    },
+
+    list: function (params, callback) {
+      fixcbparams ();
+      this.talk ('GET', '/issuers', params, callback);
+    }
+  };
+
+  this.methods = function (params, callback) {
+    fixcbparams ();
+    this.talk ('GET', '/methods', params, callback);
+  };
+};
+
+module.exports = Mollie;
